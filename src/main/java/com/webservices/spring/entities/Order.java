212570a1cd15f5b343233c2761526aaaeea95e62
Vinicius <vinicius.tecbcc@gmail.com>
2020-02-05 16:21:50 -0300

    removendo import

--- src/main/java/com/webservices/spring/entities/Order.java
+++ src/main/java/com/webservices/spring/entities/Order.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
